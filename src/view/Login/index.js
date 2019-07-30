7a8d6633bfb8e1a7ace1f0da990f347a031f49f1
11 <841850130@qq.com>
2019-07-30 19:42:30 +0800

    '登陆页面重做,用表单修改了登陆逻辑'

--- src/view/Login/index.js
+++ src/view/Login/index.js
@@ -2,65 +2,51 @@ import React , { Component } from 'react'
 import { Form, Icon, Input, Button, message } from 'antd'
 import store from '@/store'
 
-class Login extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      userName: '',
-      password: ''
-    }
-    this.onSubmit= this.onSubmit.bind(this)
-    this.changeUserName= this.changeUserName.bind(this)
-    this.changePassword= this.changePassword.bind(this)
-  }
-  
-  onSubmit(e) {
+class LoginContainer extends Component {
+  onSubmit = e => {
     e.preventDefault()
-    if(this.state.userName === 'admin' && this.state.userName === 'admin') {
-      const action = {
-        type: 'loginChange',
-        value: true
-      } 
-      store.dispatch(action)
-      this.props.history.push('/home')
-      message.success('登陆成功')
-    }else {
-      message.error('密码错误，登陆失败')
-    }
-  }
-
-  changeUserName(e) {
-    this.setState({
-      userName: e.target.value
-    })
-  }
-
-  changePassword(e) {
-    this.setState({
-      password: e.target.value
+    this.props.form.validateFieldsAndScroll((err, values) => {
+      if (!err) {
+        console.log('Received values of form: ', values);
+        if(values.username === 'admin' && values.passworld === 'admin') {
+          const action = {
+            type: 'loginChange',
+            value: true
+          } 
+          store.dispatch(action)
+          this.props.history.push('/home')
+          message.success('登陆成功')
+        }else {
+          message.error('密码错误，登陆失败')
+        }
+      }
     })
   }
   
   render() {
+    const { getFieldDecorator } = this.props.form
     return (
       <div className='login'>
         <Form onSubmit={this.onSubmit} className="login-form">
           <Form.Item>
-            <Input
-              prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-              placeholder="用户名"
-              value={this.state.userName}
-              onChange={this.changeUserName}
-            />
+            {getFieldDecorator('username', {
+              rules: [{ required: true, message: '请输入用户名!' }],
+            })(
+              <Input
+                prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                placeholder="请输入用户名"
+              />,
+            )}
           </Form.Item>
           <Form.Item>
-            <Input
-              prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
-              type="password"
-              value={this.state.password}
-              onChange={this.changePassword}
-              placeholder="密码"
-            />
+            {getFieldDecorator('passworld', {
+              rules: [{ required: true, message: '请输入密码!' }],
+            })(
+              <Input
+                prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                placeholder="请输入密码"
+              />,
+            )}
           </Form.Item>
           <Form.Item>
             <Button type="primary" htmlType="submit" className="login-form-button">
@@ -72,5 +58,7 @@ class Login extends Component {
     )
   }
 }
+
+const Login = Form.create({ name: 'normal_login' })(LoginContainer);
 
 export default Login
