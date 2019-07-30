3977092933840cb08b6e73fe1641f53afe7e82a6
11 <841850130@qq.com>
2019-07-30 20:20:11 +0800

    '制作上传图片组件'

--- src/view/Home/AddBook/index.js
+++ src/view/Home/AddBook/index.js
@@ -1,11 +1,13 @@
 import React , { Component } from 'react'
-import { Form, Input, Icon, Button } from 'antd';
+import { Form, Input, Icon, Button, Upload, message } from 'antd';
 
 class AddBookContainer extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      name: ''
+      name: '',
+      loading: false,
+      imageUrl: ''
     }
   }
 
@@ -16,6 +18,18 @@ class AddBookContainer extends Component {
         console.log('Received values of form: ', values);
       }
     })
+  }
+
+  thumbnailUpload = info => {
+    console.log(info)
+    var reader = new FileReader();
+    reader.readAsDataURL(info.file);
+    reader.onload = data => {
+      console.log(reader.result)
+      this.setState({
+        imageUrl: reader.result
+      })
+    }
   }
 
   render() {
@@ -32,6 +46,18 @@ class AddBookContainer extends Component {
         },
       },
     }
+
+    const uploadButton = (
+      <div>
+        <Icon type={this.state.loading ? 'loading' : 'plus'} />
+        <div className="ant-upload-text">Upload</div>
+      </div>
+    )
+
+    const { imageUrl } = this.state
+
+    console.log(imageUrl)
+
     return (
       <div>
         <Form onSubmit={this.submitForm} style={{width: 500}} labelCol={{ span: 5 }} wrapperCol={{ span: 12 }}>
@@ -50,20 +76,21 @@ class AddBookContainer extends Component {
               rules: [{ required: true, message: '请输入作者名称!' }],
             })(
               <Input
-                prefix={<Icon type="book" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
                 placeholder="请输入作者名称"
               />,
             )}
           </Form.Item>
           <Form.Item label="书籍缩略图">
-            {getFieldDecorator('anthor', {
-              rules: [{ required: true, message: '请输入作者名称!' }],
-            })(
-              <Input
-                prefix={<Icon type="book" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                placeholder="请输入作者名称"
-              />,
-            )}
+            <Upload
+              name="avatar"
+              listType="picture-card"
+              className="avatar-uploader"
+              showUploadList={false}
+              customRequest={this.thumbnailUpload}
+            >
+              {imageUrl ? <img src={imageUrl} alt="avatar" style={{ width: '100%' }} /> : uploadButton}
+            </Upload>
           </Form.Item>
           <Form.Item {...tailFormItemLayout}>
             <Button type="primary" htmlType="submit" className="login-form-button">
